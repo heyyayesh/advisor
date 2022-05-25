f0e04a926dce1cd57bd697039b269335a0ea0a50
heyyayesh <biswalayesh3@gmail.com>
2022-05-25 20:29:56 +0530

    Add refresh quote button

--- src/components/Advice.js
+++ src/components/Advice.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
+import refreshIcon from "../assets/refresh.svg";
 
 const Advice = () => {
   const resolution = `${window.screen.availWidth}×${window.screen.availHeight}`;
-  const imageUrl = `https://source.unsplash.com/random/${resolution}/?motivational`;
+  const imageUrl = `https://source.unsplash.com/random/${resolution}/?superhero`;
 
   const [advice, setAdvice] = useState("");
+  const [reload, setReload] = useState(false);
+
   useEffect(() => {
     async function getAdvice() {
       const response = await fetch("https://api.adviceslip.com/advice");
@@ -13,14 +16,21 @@ const Advice = () => {
     }
 
     getAdvice();
-  }, []);
+  }, [reload]);
+
+  function refresh() {
+    setReload((prev) => !prev);
+  }
 
   return (
     <div className="container">
-      <img src={imageUrl} alt="background" />
+      <img src={imageUrl} alt="background" className="background" />
       <div className="overlay"></div>
       <div className="advice">
         {advice ? <h1>{advice}</h1> : <h1>Loading...</h1>}
+        <button onClick={refresh}>
+          <img src={refreshIcon} alt="refresh" className="refresh" />
+        </button>
       </div>
     </div>
   );
